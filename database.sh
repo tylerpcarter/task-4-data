11b1f312bb843f6ea477946772f10c7bdcac1f25
tylercartergse2013 <tylercartergse2013@gmail.com>
2017-07-25 12:20:44 -0400

    changed database script

--- database.sh
+++ database.sh
@@ -15,4 +15,16 @@ sudo cp ./temp.csv /var/lib/mysql-files/
 checkDB=`mysql -u"$MYSQL" -p"$MYSQL" -e "show databases;" | grep -Fo $DATABASE`
 
 #Conditional Statement to Create Database if it doesn't already exist
-if 
+if [ "$checkDB" == "$DATABASE"]; then
+echo "Database exists, continuing without creating new database."
+
+else
+echo "Creating database named "$DATABASE""
+mysql -u"$MYSQL" -p"$MYSQL" -e "CREATE DATABASE $DATABASE"
+
+fi
+
+#Check if table exists, if not, create table
+checkTable=`mysql -u"$MYSQL" -p"$MYSQL" -e "show tables;" $DATABASE | grep -Fo $TABLE`
+
+#Conditional statement to create table if it doesn't already exist
