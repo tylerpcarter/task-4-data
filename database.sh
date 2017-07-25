0081a3c4144e1d78a3e52504eec49581c20ef629
tylercartergse2013 <tylercartergse2013@gmail.com>
2017-07-25 12:37:11 -0400

    changed script

--- database.sh
+++ database.sh
@@ -28,12 +28,12 @@ fi
 checkTable=`mysql -u"$MYSQL" -p"$MYSQL" -e "show tables;" $DATABASE | grep -Fo $TABLE`
 
 #Conditional statement to create table if it doesn't already exist
-if [ "$DBCHECK" == "$MYTABLE" ]; then
+if [ "$checkTable" == "$TABLE" ]; then
    echo "Table exists"
    
 else
    echo "Table does not exist. Creating table..."
-   mysql -u"$MYSQL" -p"$MYSQL" -e "CREATE TABLE $TABLE (ID VARCHAR(255), Date TIMESTAMP, HomeCapital VARCHAR(255), BAC_Limit VARCHAR(255), Fav_Superhero VARCHAR(255), First_VidGame NUMERIC(5,2), %GermElimByHandSanitizer VARCHAR(255)); ALTER TABLE $TABLE ADD PRIMARY KEY (ID);" $DATABASE
+   mysql -u"$MYSQL" -p"$MYSQL" -e "CREATE TABLE $TABLE (ID VARCHAR(255), Date TIMESTAMP, HomeCapital VARCHAR(255), BAC_Limit VARCHAR(255), Fav_Superhero VARCHAR(255), First_VidGame NUMERIC(5,2), GermsElimByHandSanitizer VARCHAR(255)); ALTER TABLE $TABLE ADD PRIMARY KEY (ID);" $DATABASE
    
 fi
 
